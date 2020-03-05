28d96b0eab157b41645e741ef728bbfcc96a96e8
Goncalo Batalhao Alves <up201806451@FEUPSIG.FE.UP.PT>
2020-03-05 18:34:03 +0000

    Added color

--- ex2/MyChair.js
+++ ex2/MyChair.js
@@ -3,25 +3,36 @@
  * @constructor
  * @param scene - Reference to MyScene object
  */
-class MyTable extends CGFobject {
+class MyChair extends CGFobject {
 	constructor(scene) {
 		super(scene);
-        this.tabletop = new MyUnitCube(this.scene);
+        this.chairbase = new MyUnitCube(this.scene);
+        this.chairback = new MyUnitCube(this.scene);
         this.frleg = new MyLeg(this.scene);
         this.flleg = new MyLeg(this.scene);
         this.brleg = new MyLeg(this.scene);
         this.blleg = new MyLeg(this.scene);
 	}
 	display() {
-        
+        //Scaling Chair
+        this.scene.pushMatrix();
+        this.scene.scale(0.5,0.5,0.5);
+        this.scene.setDiffuse(1,0,0,0);
         //Puts base of chair in xz plane
         this.scene.pushMatrix();
         this.scene.translate(0,5.25,0);
-        this.scene.setDiffuse(0,1,0,0);
-        //TableTop
+        //Chair Base
         this.scene.pushMatrix();
-        this.scene.scale(10,0.5,10);
-        this.tabletop.display();
+        this.scene.scale(10,1.5,10);
+        this.chairbase.display();
+        this.scene.popMatrix();
+
+        //Chair Back
+        this.scene.pushMatrix();
+        this.scene.translate(0,8.25,-4.5);
+        this.scene.rotate(Math.PI/2,1,0,0);
+        this.scene.scale(10,1,15);
+        this.chairback.display();
         this.scene.popMatrix();
 
         //Front Left Leg
@@ -48,5 +59,7 @@ class MyTable extends CGFobject {
         this.flleg.display();
         this.scene.popMatrix();
         this.scene.popMatrix();
+        this.scene.popMatrix();
+        
     }
 }
