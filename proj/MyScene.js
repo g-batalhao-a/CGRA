603ab6afccdf7b869ee18b0f99dea4324795d730
Pedro Seixas <49153615+pedrojfs17@users.noreply.github.com>
2020-04-16 18:15:13 +0100

    Fixed lights

--- proj/MyScene.js
+++ proj/MyScene.js
@@ -72,6 +72,7 @@ class MyScene extends CGFscene {
         };
     }
     initLights() {
+        this.setGlobalAmbientLight(0.5, 0.5, 0.5, 1.0);
         this.lights[0].setPosition(15, 2, 5, 1);
         this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
         this.lights[0].enable();
